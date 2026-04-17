4bebbff8dcb1e17d5e8440949040c99c5c01e0a8
Your Name <88072010+Razshy@users.noreply.github.com>
2026-04-17 04:17:11 -0500

    Clean: Ext

--- src/vs/workbench/services/extensionManagement/common/extensionManagementServerService.ts
+++ src/vs/workbench/services/extensionManagement/common/extensionManagementServerService.ts
@@ -9,16 +9,11 @@ import {
 	IExtensionManagementServer,
 	IExtensionManagementServerService
 } from './extensionManagement.js';
-import { IRemoteAgentService } from '../../remote/common/remoteAgentService.js';
-import { Schemas } from '../../../../base/common/network.js';
-import { IChannel } from '../../../../base/parts/ipc/common/ipc.js';
 import { InstantiationType, registerSingleton } from '../../../../platform/instantiation/common/extensions.js';
-import { ILabelService } from '../../../../platform/label/common/label.js';
 import { isWeb } from '../../../../base/common/platform.js';
 import { IInstantiationService } from '../../../../platform/instantiation/common/instantiation.js';
 import { WebExtensionManagementService } from './webExtensionManagementService.js';
 import { IExtension } from '../../../../platform/extensions/common/extensions.js';
-import { RemoteExtensionManagementService } from './remoteExtensionManagementService.js';
 
 export class ExtensionManagementServerService implements IExtensionManagementServerService {
 	declare readonly _serviceBrand: undefined;
@@ -28,27 +23,8 @@ export class ExtensionManagementServerService implements IExtensionManagementSer
 	readonly webExtensionManagementServer: IExtensionManagementServer | null = null;
 
 	constructor(
-		@IRemoteAgentService remoteAgentService: IRemoteAgentService,
-		@ILabelService labelService: ILabelService,
 		@IInstantiationService instantiationService: IInstantiationService
 	) {
-		const remoteAgentConnection = remoteAgentService.getConnection();
-		if (remoteAgentConnection) {
-			const extensionManagementService = instantiationService.createInstance(
-				RemoteExtensionManagementService,
-				remoteAgentConnection.getChannel<IChannel>('extensions')
-			);
-			this.remoteExtensionManagementServer = {
-				id: 'remote',
-				extensionManagementService,
-				get label() {
-					return (
-						labelService.getHostLabel(Schemas.vscodeRemote, remoteAgentConnection.remoteAuthority) ||
-						localize('remote', 'Remote')
-					);
-				}
-			};
-		}
 		if (isWeb) {
 			const extensionManagementService = instantiationService.createInstance(WebExtensionManagementService);
 			this.webExtensionManagementServer = {
@@ -60,20 +36,14 @@ export class ExtensionManagementServerService implements IExtensionManagementSer
 	}
 
 	getExtensionManagementServer(extension: IExtension): IExtensionManagementServer {
-		if (extension.location.scheme === Schemas.vscodeRemote) {
-			return this.remoteExtensionManagementServer!;
-		}
 		if (this.webExtensionManagementServer) {
 			return this.webExtensionManagementServer;
 		}
 		throw new Error(`Invalid Extension ${extension.location}`);
 	}
 
-	getExtensionInstallLocation(extension: IExtension): ExtensionInstallLocation | null {
-		const server = this.getExtensionManagementServer(extension);
-		return server === this.remoteExtensionManagementServer
-			? ExtensionInstallLocation.Remote
-			: ExtensionInstallLocation.Web;
+	getExtensionInstallLocation(_extension: IExtension): ExtensionInstallLocation | null {
+		return ExtensionInstallLocation.Web;
 	}
 }
 
